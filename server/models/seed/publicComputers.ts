c5227933b6d1688eca58822f670b504da736125b
Aaron Bart-Addison <abaddison16@gmail.com>
2022-02-28 16:40:41 -0500

    Add publicly available computers file

--- server/models/seed/publicComputers.ts
+++ server/models/seed/publicComputers.ts
@@ -1,1 +1,15 @@
 
+export default [
+  {
+    name: "NYPL 53 St",
+    address: "18 W 53rd St, New York, NY 10019",
+    borough: "Bronx",
+    operationTime: "Tue-Sat 11 AM- 6PM",
+    url: "https://www.nypl.org/locations/53rd-street",
+    email: "53stbranch@nypl.org",
+    contact_number: "212-714-8400",
+    lat: 40.7607991,
+    long: -73.9770826,
+    tags: ["Youth", "Education", "Accessible"]
+  },
+];
