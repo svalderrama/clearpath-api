d64980a20f323e1f87ce9d1c34c21a95ecf9f3f0
Aaron Bart-Addison <abaddison16@gmail.com>
2022-02-28 16:49:01 -0500

    Update publicBathrooms.ts

--- server/models/seed/publicBathrooms.ts
+++ server/models/seed/publicBathrooms.ts
@@ -4,7 +4,7 @@ export default [
     address: "Bronx Boulevard between East 226 and East 228 streets",
     borough: "Bronx",
     operationTime: "6am - 10pm",
-    facilityType: "Park"
+    facilityType: "Park",
     lat: 40.888232,
     long: -73.8526748,
     tags: ["Bathrooms"]
