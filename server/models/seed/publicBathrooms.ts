80aef5b4d50ef89ade8c6951f47763bfa5ba79f4
Aaron Bart-Addison <abaddison16@gmail.com>
2022-02-28 16:48:26 -0500

    Create publicBathrooms.ts

---
+++ server/models/seed/publicBathrooms.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: "Bronx River Parkway (227th Street Playground)",
+    address: "Bronx Boulevard between East 226 and East 228 streets",
+    borough: "Bronx",
+    operationTime: "6am - 10pm",
+    facilityType: "Park"
+    lat: 40.888232,
+    long: -73.8526748,
+    tags: ["Bathrooms"]
+  },
+];
